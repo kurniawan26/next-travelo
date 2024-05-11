f484f45d1d5faa09296d2ac993cb631a45330e4f
Fauzi Kurniawan <71652809+kurniawan26@users.noreply.github.com>
2024-05-11 09:16:11 +0700

    feat(module-1): Learn not found concept

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className='container justify-center flex items-center h-screen'>
+      <p className='text-4xl'>Not Found Page</p>
+    </div>
+  )
+}
+
+export default NotFound

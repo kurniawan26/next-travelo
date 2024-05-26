a12f706f33e6b266e90f0afe3fda8d73e9f7eeaf
Fauzi Kurniawan <71652809+kurniawan26@users.noreply.github.com>
2024-05-26 14:07:58 +0700

    feat(module-1): Add favicon and metadata to layout and login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,2 +1,10 @@
 import "./globals.css";
 export { default } from '@/components/Layouts/RootLayout'
+
+export const metadata = {
+  title: {
+    template: "%s / Travelo",
+    default: "Welcome / Travelo"
+  },
+  description: "Sebuah aplikasi website untuk kebutuhan liburan kamu"
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,5 @@
 export { default } from '@/components/Pages/LoginPage'
+
+export const metadata = {
+  title: "Login"
+}
